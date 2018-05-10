4fb4e0004152f7b0acc2bd946422ec6cbd43b966
Dominic <hi@ydcool.me>
2018-05-10 17:32:41 +0800

    update;

--- src/roscontrol/lib/initialPose.js
+++ src/roscontrol/lib/initialPose.js
@@ -26,11 +26,11 @@ function setUpCommand(rosNode) {
 
 /**
  * 发送 cmd_vel 指令，控制机器人运动
- * @param pose 带有位置信息的 postStamped msg
+ * @param inPoseStamped 带有位置信息的 postStamped msg
  * @param angel 前端设定的方向 （角度）
  * @param rosNode 宿主node(tr_hmi_node)
  */
-function pubInitialPose(pose, angle, rosNode) {
+function pubInitialPose(inPoseStamped, angle, rosNode) {
     if (!rosNode) {
         rosNode = rosnodejs.nh;
         if (!rosNode) {
@@ -43,10 +43,9 @@ function pubInitialPose(pose, angle, rosNode) {
     }
 
     /*
- { pose:                                                                                                            
-     { header: { seq: 2, stamp: [Object], frame_id: 'map' },                                                                            
+ { inPoseStamped: { header: { seq: 2, stamp: [Object], frame_id: 'map' },                                                                            
        pose: { position: [Object], orientation: [Object] } },                                                                           
-    angle: 244.6875 }       
+   angle: 244.6875 }       
     
   publishing /initialPose: PoseWithCovarianceStamped {                                                                                  
     header: Header { seq: 0, stamp: { secs: 0, nsecs: 0 }, frame_id: '' },                                                             
@@ -58,24 +57,50 @@ function pubInitialPose(pose, angle, rosNode) {
      { header: { seq: 2, stamp: [Object], frame_id: 'map' },                                                                            
        pose: { position: [Object], orientation: [Object] } },                                                                           
     angle: 244.6875 } 
+
+    {
+        "header":{"frame_id":"map"},
+        "pose":{"pose":{ "position":{"x":-1.3224197995196034,"y":0.31743787616173136,"z":0},
+                "orientation":{"x":0,"y":0,"z":0,"w":1}},
+                "covariance":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        }
+    }
+
+header: 
+  seq: 11
+  stamp: 
+    secs: 1525943368
+    nsecs: 593990501
+  frame_id: "map"
+pose: 
+  pose: 
+    position: 
+      x: -3.60285520554
+      y: 4.21816062927
+      z: 0.0
+    orientation: 
+      x: 0.0
+      y: 0.0
+      z: 0.801300946497
+      w: 0.598261475563
+  covariance: [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]
+
     */
 
     //TODO: convert angle to orientation(geometry_msgs/Quaternion.msg)
     //angle => _covariance
-    
-    let _covariance = new Float64Array([]);
 
-    pose.pose.orientation.w = 1;
+    let _covariance = new Float64Array();
+
+    inPoseStamped.pose.orientation.w = 1;
 
     let _poseWithCovariance = new PoseWithCovariance({
-        header: pose.header,
-        pose: pose.pose
+        pose: inPoseStamped.pose,
+        covariance: _covariance
     });
 
     let _poseWithCovarianceStamped = new PoseWithCovarianceStamped({
-        header: {
-            frame_id: "map"
-        },
+        header: inPoseStamped.header,
         pose: _poseWithCovariance
     });
 
