a874e78dd650a3b4b7a3c057c2d4b342d855c5a1
Dominic <hi@ydcool.me>
2018-06-06 17:53:43 +0800

    add LaserScan data visualization

---
+++ src/public/map/js/LaserShape.js
@@ -0,0 +1,53 @@
+/**
+ * @author Dominic.Y - hi@ydcool.me
+ */
+
+/**
+ * A shape to draw a sensor_msgs/LaserScan msg
+ *
+ * @constructor
+ * @param options - object with following keys:
+ *   * message  - sensor_msgs/LaserScan message
+ *   * colorfulIntensity (optional) - use colorful points to indentify intensity
+ *   * pointSize (optional) - the size of the point
+ *   * pointColor (optional) - the createjs color for the point
+ *   * rotation (optional) - rotation of laser from tf transmations
+ *   * translation (optional) - translation of laser from tf transmations
+ */
+ROS2D.LaserShape = function (options) {
+    options = options || {};
+    this.message = options.message;
+    this.scale = options.scale || 1;
+    this.colorfulIntensity = options.colorfulIntensity || false;
+    this.pointSize = options.pointSize || 1;
+    this.pointColor = options.pointColor || createjs.Graphics.getRGB(255, 0, 0);
+    this.transform = options.transform;
+
+    this.graphics = new createjs.Graphics();
+
+    var currRotation = this.transform ? createjs.Stage.prototype.rosQuaternionToGlobalTheta.call(this, this.transform.rotation) : 0;
+
+    var tlX = this.transform ? this.transform.translation.x : 0;
+    var tlY = this.transform ? this.transform.translation.y : 0;
+
+    var ranges = this.message.ranges;
+
+    for (var i = 0; i < ranges.length; i++) {
+
+        var r = ranges[i];
+
+        if (r !== null && r >= this.message.range_min && r <= this.message.range_max) {
+            var t = Math.PI + currRotation - (i * this.message.angle_increment + this.message.angle_min);
+
+            var x = r * Math.cos(t);
+            var y = r * Math.sin(t);
+
+            this.graphics.beginFill(this.pointColor).drawCircle(x + tlX, y - tlY, this.pointSize / this.scale);
+        }
+    }
+
+    // create the shape
+    createjs.Shape.call(this, this.graphics);
+};
+
+ROS2D.LaserShape.prototype.__proto__ = createjs.Shape.prototype;
