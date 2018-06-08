d065a4e4724288a104479e9728cb9d03c07e59dd
Dominic <hi@ydcool.me>
2018-06-08 17:33:23 +0800

    add get robot info api (product info)

--- src/restful/controllers/status.js
+++ src/restful/controllers/status.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const db = require('../../utils/singletonDB').getDB();
+
 /**
  * 系统使用状态统计
  */
@@ -33,7 +35,33 @@ let fn_rosStatus = async (ctx, next) => {
     }
 }
 
+/**
+ * 
+ */
+let fn_robotInfo = async (ctx, next) => {
+    try {
+        let product = db.get('product').value();
+        let productInfo = db.get('productions').find({
+            name: product
+        }).value();
+
+        ctx.response.body = {
+            message: 'success',
+            data: {
+                product: product,
+                productInfo: productInfo
+            }
+        }
+    } catch (e) {
+        console.error('failed to get robot info:', e);
+        ctx.response.body = {
+            message: `failed:${e}`
+        }
+    }
+}
+
 module.exports = {
     'GET /status/systemInfo': fn_systemInfo,
     'GET /status/ros': fn_rosStatus,
+    'GET /status/robot': fn_robotInfo,
 }
